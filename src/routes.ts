29fd2a6103b1e6a4cd3997c5605b55e3088c1ca5
Agata Kamińska <delicadency@gmail.com>
2025-08-02 19:55:09 +0200

    feat: routing created

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+export const Routes = {
+  HOME: "/",
+  GALLERY: "/galeria",
+  REGULATIONS: "/regulamin",
+  POLICY: "/polityka-prywatnosci",
+  CONTACT: "/kontakt",
+  
+} as const;
+
+export type RouteKeys = keyof typeof Routes;
+export type RouteValues = (typeof Routes)[RouteKeys];
