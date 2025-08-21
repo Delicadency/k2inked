bd9f79854ac16105121d3aa8bfb65b6d7f1da303
Agata Kamińska <delicadency@gmail.com>
2025-08-21 13:20:16 +0200

    feat: Heading component created

---
+++ src/components/Heading.tsx
@@ -0,0 +1,42 @@
+import { cn } from "@/utils";
+import { ElementType, ReactNode } from "react";
+
+type HeadingProps = {
+  children: ReactNode;
+  as?: ElementType;
+  className?: string;
+  variant: "light" | "dark";
+  tag: "h1" | "h2" | "h3";
+};
+
+export const Heading: React.FC<HeadingProps> = ({
+  tag,
+  as: Heading = tag,
+  className = "",
+  children,
+  variant,
+}) => {
+  const variants = {
+    light: "text-light",
+    dark: "text-dark",
+  };
+
+  const sizes = {
+    h1: "tablet:text-7xl text-[12vw]",
+    h2: "tablet:text-[52px] text-[8vw]",
+    h3: "tablet:text-[32px] text-[6vw]",
+  };
+
+  return (
+    <Heading
+      className={cn(
+        "font-marcellus-sc tracking-[0.15em]",
+        variants[variant],
+        sizes[tag],
+        className,
+      )}
+    >
+      {children}
+    </Heading>
+  );
+};
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,6 @@
 import { LogoK2Inked } from "@/icons/LogoK2Inked";
 import { BackgroundImage } from "./BackgroundImage";
+import { Heading } from "./Heading";
 
 export const Hero = () => (
   <section className="">
@@ -9,7 +10,9 @@ export const Hero = () => (
       priority
     >
       <div className="flex flex-col items-center justify-center tablet:h-[100vh] tablet:w-full">
-        <LogoK2Inked classname="text-light max-w-[80vw] tablet:max-w-[500px]" />
+        <LogoK2Inked classname="text-light max-w-[80vw] mobile:max-w-[80vw] tablet:max-w-[500px]" />
+        <Heading tag="h1" variant="light">K2.inked</Heading>
+        <Heading tag="h3" variant="light">Studio w sercu Warszawy</Heading>
       </div>
     </BackgroundImage>
   </section>
