a1dbf51fb9569418d1f748390171e7c84832f4ef
Agata Kamińska <delicadency@gmail.com>
2025-08-25 22:17:04 +0200

    chore: K2INKED_DATA updated

--- src/data/k2Data.ts
+++ src/data/k2Data.ts
@@ -1,10 +1,10 @@
 export const K2INKED_DATA = {
   street: "Krucza 47A",
   city: "00-509 Warszawa",
-  phone: "",
-  email: "",
+  phone: "+48 883 308 451",
+  email: "K2inked.zapisy@gmail.com",
   socialMedia: {
-    facebook: "",
+    facebook: "https://www.facebook.com/share/16zqfQMuDb/?mibextid=wwXIfr",
     instagram: "https://www.instagram.com/k2.inked",
     tiktok: "",
   },
