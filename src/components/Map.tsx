3c776337af6caa2b36b5998fe75bd48ab377fbc9
Agata Kamińska <delicadency@gmail.com>
2025-08-31 18:36:35 +0200

    feat: reintroduce Map component with variant support for responsive design

---
+++ src/components/Map.tsx
@@ -0,0 +1,24 @@
+type VariantProps = {
+  variant: "hero" | "contact";
+};
+
+export const Map = ({ variant }: VariantProps) => {
+  const variants = {
+    hero: "",
+    contact:
+      "tablet:h-[55em] tablet:max-h-full tablet:w-[40%] relative h-96 max-h-[30%] w-full overflow-hidden rounded-tr-3xl",
+  };
+
+  return (
+    <div className={variants[variant]}>
+      <iframe
+        title="Mapa dojazdu — K2.inked"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1221.8623090166263!2d21.015671879915452!3d52.2302207963174!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471ecd88a09b3e6d%3A0xe483c785b50e377a!2sK2.inked!5e0!3m2!1spl!2spl!4v1756646777890!5m2!1spl!2spl"
+        loading="lazy"
+        allowFullScreen
+        referrerPolicy="no-referrer-when-downgrade"
+        className="absolute inset-0 h-full w-full border-0 grayscale filter transition duration-500 hover:grayscale-0 active:grayscale-0"
+      />
+    </div>
+  );
+};
