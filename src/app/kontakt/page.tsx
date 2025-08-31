9f646c6f41a258a674faa32464a4b7381cd40a89
Agata Kamińska <delicadency@gmail.com>
2025-08-31 18:36:52 +0200

    feat: implement ContactPage with Map and Form components

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,5 +1,7 @@
 import { PageLayout } from "@/components/PageLayout";
 import { contactData } from "./data";
+import { Map } from "../../components/Map";
+import { Form } from "./components/Form";
 
 const ContactPage = () => {
   return (
@@ -8,13 +10,18 @@ const ContactPage = () => {
       headingName="Napisz do nas"
       tabDeskClassName="tablet:w-180 desktop:w-180"
     >
-        <article className="tablet:text-lg mb-25 text-base leading-7.5 font-light tracking-[0.12em]">
+      <article className="tablet:text-lg mb-25 text-base leading-7.5 font-light tracking-[0.12em]">
         {contactData.map((line, i) => (
           <p className="mb-6" key={i}>
             {line}
           </p>
         ))}
-        </article>
+      </article>
+
+      <section className="bg-secondary drop-shadow-3xl/40 tablet:flex-row-reverse tablet:w-full mb-20 flex h-fit w-[90vw] flex-col items-center rounded-tr-3xl rounded-bl-3xl">
+        <Map variant="contact" />
+        <Form />
+      </section>
     </PageLayout>
   );
 };
