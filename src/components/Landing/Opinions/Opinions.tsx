4aab5deea382dfc1a688009a49fd15f5e3f0b0ad
Agata Kamińska <delicadency@gmail.com>
2025-09-06 19:39:53 +0200

    feat: add placeholder testimonials and images to Opinions component for enhanced user feedback

--- src/components/Landing/Opinions/Opinions.tsx
+++ src/components/Landing/Opinions/Opinions.tsx
@@ -8,8 +8,7 @@ export const Opinions = () => {
       variant="light"
       tabDeskClassName="tablet:w-170 desktop:w-170"
     >
-      <article className="text-dark/80 tablet:text-lg tablet:leading-9 mx-auto max-w-5xl px-6 text-center leading-8 font-light tracking-widest">
-        {" "}
+      <article className="text-dark/80 tablet:text-lg tablet:leading-9 tablet:text-center mx-auto max-w-5xl px-6 text-start text-sm leading-7 font-light tracking-widest">
         {paragraphs.map((p, i) => (
           <p key={i} className="mb-6 last:mb-0">
             {p}
