f7ff321f0833094f75142d725a1a775515d11fbc
{Agata Kuklińska} <delicadency@gmail.com>
2025-09-20 17:53:47 +0200

    feat: update team paragraphs with unique studio descriptions and services offered

--- src/components/Landing/Team/data.ts
+++ src/components/Landing/Team/data.ts
@@ -1,6 +1,6 @@
 type TeamParagraphs = string[];
 
 export const teamParagraphs: TeamParagraphs = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus fermentum sem, non malesuada ante volutpat in. Donec lorem magna, scelerisque congue ligula a, ultricies dignissim nisl. Aliquam feugiat nisl dui, in semper nisi sodales eget. Morbi et lacus at risus sollicitudin semper semper eget ante. Nulla molestie placerat libero vitae venenatis. Ut urna justo, placerat at lobortis quis, auctor et justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
-  "In hac habitasse platea dictumst. Aenean a quam at erat efficitur fringilla. Nullam euismod, nisi vel consectetur interdum, nisl nisi aliquet nunc, nec lacinia nunc nisl nec nisi. Sed euismod, nisl vel consectetur interdum, nisl nisi aliquet nunc, nec lacinia nunc nisl nec nisi. Sed euismod, nisl vel consectetur interdum, nisl nisi aliquet nunc, nec lacinia nunc nisl nec nisi.",
+  "Każda z nas ma swój unikalny styl i podejście do tatuażu, co pozwala nam sprostać różnorodnym oczekiwaniom naszych klientów. W naszym zespole znajdziesz artystki specjalizujące się w różnych technikach, od delikatnych linii po realistyczne portrety.",
+  "Oferujemy nie tylko tatuaże, ale także profesjonalny piercing, który podkreśli Twój indywidualny styl. Nasze studio to miejsce, gdzie pasja do sztuki spotyka się z najwyższymi standardami higieny i bezpieczeństwa.",
 ];
